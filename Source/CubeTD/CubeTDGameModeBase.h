3bf9fec1711fac103212928295b16b9fd46ba9b9
Xiul <xiul10901@gmail.com>
2019-06-07 15:56:11 +0200

    Initial commit

---
+++ Source/CubeTD/CubeTDGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CubeTDGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CUBETD_API ACubeTDGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
