351553d1437912e3181564d84ca30337bb63bb30
Xiul <xiul10901@gmail.com>
2019-07-01 14:17:11 +0200

    Implemented Nexus, which controls the remaining life.

--- Source/CubeTD/Structures/Nexus.cpp
+++ Source/CubeTD/Structures/Nexus.cpp
@@ -2,10 +2,40 @@
 
 #include "Nexus.h"
 
+#include "Enemies/BaseEnemy.h"
+#include "Enemies/DivisibleEnemy.h"
 
 
 void ANexus::OnConstruction(const FTransform & Transform)
 {
 	Super::OnConstruction(Transform);
+	
 	Navigable = true;
+	CurrentLife = MaxLife;
+}
+
+void ANexus::ApplyDamage(float Damage)
+{
+	CurrentLife -= Damage;
+	
+	OnDamageDone.Broadcast(Damage, CurrentLife);
+}
+
+void ANexus::BeginOverlap(AActor * OverlappedActor, AActor * OtherActor)
+{
+	ABaseEnemy* Enemy = Cast<ABaseEnemy>(OtherActor);
+	if (Enemy) {
+		ApplyDamage(Enemy->Damage);
+
+		ADivisibleEnemy* DivisibleEnemy = Cast<ADivisibleEnemy>(Enemy);
+		if (DivisibleEnemy)
+			DivisibleEnemy->SubdivisionsLeft = 0;
+
+		Enemy->Destroy();
+	}
+}
+
+void ANexus::BeginPlay()
+{
+	OnActorBeginOverlap.AddDynamic(this, &ANexus::BeginOverlap);
 }
--- Source/CubeTD/Structures/Nexus.h
+++ Source/CubeTD/Structures/Nexus.h
@@ -13,8 +13,26 @@ UCLASS()
 class CUBETD_API ANexus : public ABasicStructure
 {
 	GENERATED_BODY()
-	
+
+		DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FDamageDoneDelegate, float, DamageDone, float, CurrentLife);
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float MaxLife;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	float CurrentLife;
+
+	UPROPERTY(BlueprintAssignable)
+	FDamageDoneDelegate OnDamageDone;
 public:
 	virtual void OnConstruction(const FTransform & Transform) override;
+
+	UFUNCTION(BlueprintCallable)
+	void ApplyDamage(float Damage);
 	
+protected:
+	UFUNCTION()
+	void BeginOverlap(AActor* OverlappedActor, AActor* OtherActor);
+
+	virtual void BeginPlay() override;
 };
